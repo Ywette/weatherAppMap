59c9aee4f1a1256be0b922cc9c0dfb71781e7564
Iveta Staune <iveta.staune@gmail.com>
2022-01-26 19:08:48 +0200

    .

---
+++ apikey.js
@@ -0,0 +1,2 @@
+const API_KEY="a8e71c9932b20c4ceb0aed183e6a83bb"
+export default API_KEY;
--- script.js
+++ script.js
@@ -1,109 +1,93 @@
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
-/**
- * Retrieve weather data from openweathermap
- */
-getWeatherData = (city) => {
+import API_KEY from "./apikey";
+
+const getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
-  //HINT: Use template literals to create a url with input and an API key
   const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=metric`;
-  
   const weatherPromise = fetch(FULL_URL);
-  return weatherPromise.then((response)=>{ 
-      //returns promise with data
-    return response.json();
-  })  
+
+  return weatherPromise
+      .then((response)=>{
+        if(response.status === 404){
+          document.getElementById("city-name").innerHTML = "There is no data about this city";
+s        }
+        return response.json();
+      })
 }
 
-searchCity = () => {
-  var city = document.getElementById('city-input').value;
-   getWeatherData(city)
-   .then((city)=>{ //now we have all data as an object of city (when write city in searchbox)
-                        //when have response, use function to show data on html    
-      showWeatherData(city);
-      // console.log(city.coord.lat, city.coord.lon)
-   }).catch((error)=>{
-    console.log(error);
-  })
+const searchCity = () => {
+  let city = document.getElementById('city-input').value;
+  getWeatherData(city)
+      .then((city)=>{
+        showWeatherData(city);
+        console.log(city)
+      })
 }
 
-// Show the weather data in HTML 
-showWeatherData = (weatherData) => {  
+const showWeatherData = (weatherData) => {
   document.getElementById("city-name").innerHTML = weatherData.name;
   document.getElementById("weather-type").innerHTML = weatherData.weather[0].main;
   document.getElementById("temp").innerHTML = weatherData.main.temp;
   document.getElementById("min-temp").innerHTML = weatherData.main.temp_min;
-  document.getElementById("max-temp").innerHTML = weatherData.main.temp_max;  
-    //change background
-  
-  let background = document.querySelector('main');
+  document.getElementById("max-temp").innerHTML = weatherData.main.temp_max;
+
+  //change backgrounds
+  const background = document.querySelector('main');
   let weatherType = weatherData.weather[0].main;
 
-
-  if ( weatherType == "Clear"){
-    background.removeAttribute("class");
-    background.classList.add("clear");    
-  }else if( weatherType == "Snow"){
-    background.removeAttribute("class");
-
+  background.removeAttribute("class");
+  if (weatherType === "Clear"){
+    background.classList.add("clear");
+  }else if( weatherType === "Snow"){
     background.classList.add("snow");
-  }else if( weatherType == "Mist"){
-    background.removeAttribute("class");
-
+  }else if( weatherType === "Mist"){
     background.classList.add("mist");
-  }else if( weatherType == "Rain"){
-    background.removeAttribute("class");
-
-    background.classList.add("rain"); 
-  }else if( weatherType == "Clouds"){
-    background.removeAttribute("class");
-
+  }else if( weatherType === "Rain"){
+    background.classList.add("rain");
+  }else if( weatherType === "Clouds"){
     background.classList.add("clouds");
   }
 }
 
-createMarker = () => { 
-  var city = document.getElementById('city-input').value;
+const createMarker = () => {
+  let city = document.getElementById('city-input').value;
   getWeatherData(city)
-  .then((city)=>{ 
-        marker = new google.maps.Marker({
+  .then((city)=>{
+        let marker = new google.maps.Marker({
         draggable: true,
         animation: google.maps.Animation.DROP,
         position: {
-          lat: city.coord.lat, 
+          lat: city.coord.lat,
           lng: city.coord.lon
-        },        
-        map: map    
-      });      
+        },
+        map: map
+      });
     }).catch((error)=>{
     console.log(error);
-    }) 
+    })
 }
 
-var input = document.getElementById("city-input");
+let input = document.getElementById("city-input");
 input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {    
+  if (event.keyCode === 13) {
     createMarker();
-    searchCity();     
+    searchCity();
     input.value = "";
-  };    
+  };
 });
 
+let map;
 
-let currentTime = new Date();
-console.log(currentTime);
-
-var map;
-var latlng;
-function initMap() {  
-  var mapCenter = {  
+function initMap() {
+  let  mapCenter = {
   lat: 33.589886,
   lng: -7.603869
   }
+
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 1,
     center: mapCenter,
-    styles: mapStyle,  
+    styles: mapStyle,
     disableDefaultUI: true
   });
 }
